f81b2b54a5ec18f1e1ee3cf05b67423d81f9ba65
Doesystem <doe.system@gmail.com>
2023-07-21 03:40:09 +0700

    set style and store endpoint api

--- script/index.js
+++ script/index.js
@@ -7,14 +7,25 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     let urlDisplay = document.getElementById('urlDisplay');
     urlDisplay.textContent = url;
 
+    let isYoutubeSite = isYouTubeWebsite();
     let isYoutube = document.getElementById('isYoutube');
-    isYoutube.textContent = isYouTubeWebsite()
+    isYoutube.textContent = isYoutubeSite
 
     id = getYouTubeIDFromURL()
     let youtubeIds = document.getElementsByClassName('youtubeId');
     for(let i = 0; i < youtubeIds.length; i++){
         youtubeIds[i].textContent = id
     }
+
+    if(!id){
+        const submit = document.getElementById('submit-form')
+        submit.disabled = true;
+    }
+
+    const apiendpointEle = document.getElementById('api-to-call');
+    apiendpointEle.addEventListener("blur", storeEndpoint);
+    apiendpointEle.value = localStorage.getItem('apiendpoint');
+
 });
 
 function isYouTubeWebsite() {
@@ -41,4 +52,9 @@ function getYouTubeIDFromURL() {
     } else {
         return null;
     }
+}
+
+function storeEndpoint(event) {
+    const inputValue = event.target.value;
+    localStorage.setItem('apiendpoint', inputValue);
 }
